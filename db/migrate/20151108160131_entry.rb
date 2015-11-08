dcab1fe0eb67989657f1559489b86ec6f8bd4b50
shts <shts.dev@gmail.com>
2015-11-09 01:13:56 +0900

    add migration file

--- db/migrate/20151108160131_entry.rb
+++ db/migrate/20151108160131_entry.rb
@@ -1,4 +1,12 @@
 class Entry < ActiveRecord::Migration
-  def change
+  def up
+    create_table :entries do |t|
+      t.string  :url
+      t.timestamps null: false
+    end
+  end
+
+  def down
+    drop_table :entries
   end
 end
